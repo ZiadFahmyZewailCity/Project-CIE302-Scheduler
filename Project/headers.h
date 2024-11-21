0345ac78e416244cf5d20f00507df5549f43b712
AhmedsElysium <ahmadyassermo@gmail.com>
2024-11-21 21:03:01 +0200

    Reformatting and Moving Loader to Header File

--- Project/headers.h
+++ Project/headers.h
@@ -1,15 +1,15 @@
-#include <stdio.h>      //if you don't use scanf/printf change this include
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <signal.h>
+#include <stdio.h> //if you don't use scanf/printf change this include
+#include <stdlib.h>
 #include <sys/file.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
 #include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 
 typedef short bool;
 #define true 1
@@ -17,65 +17,108 @@ typedef short bool;
 
 #define SHKEY 300
 
-//Process data structure
-struct processData{
-    unsigned int id;
-    unsigned int arrivalTime;
-    unsigned int runTime;
-    unsigned int priority;
+// Process data structure
+struct processData {
+  unsigned int id;
+  unsigned int arrivalTime;
+  unsigned int runTime;
+  unsigned int priority;
 };
 
-enum schedulingAlgorithm{
-    SJF=1,
-    PHPF,
-    RR
-};
+enum schedulingAlgorithm { SJF = 1, PHPF, RR };
 
+// Process Data Loader
+struct processData *load(char *inpFileName) {
+  int count_processes = 0;
+  struct processData *p_arr_process;
+
+  // Opening File and checking if its been successfully opened
+  FILE *p_file = fopen(inpFileName, "r");
+  if (p_file == NULL) {
+    perror("ERROR HAS OCCURED IN INPUT FILE OPENINING");
+    fclose(p_file);
+    return NULL;
+  }
+
+  // Buffer needed to store content of line (This is needed even we dont need
+  // the content while counting the number of processes)
+  char buffer[256];
+
+  // skipping past comments line
+  fgets(buffer, sizeof(buffer), p_file);
+
+  // itterating through the file to find number of processes
+  while (fgets(buffer, sizeof(buffer), p_file) != NULL) {
+    count_processes += 1;
+  }
+
+  // once counting is complete we close the file to now read the file data
+  fclose(p_file);
+
+  // Dyanmic allocation of array for processes
+  p_arr_process = (struct processData *)malloc(count_processes *
+                                               sizeof(struct processData));
+
+  // Reopening input file
+  p_file = fopen(inpFileName, "r");
+  if (p_file == NULL) {
+    perror("ERROR HAS OCCURED IN INPUT FILE OPENINING _2_");
+    fclose(p_file);
+    return NULL;
+  }
+
+  // skipping past comments line
+  fgets(buffer, sizeof(buffer), p_file);
+
+  // itterating through file to get data of each
+  for (int i = 0; i < count_processes; i++) {
+    if (fscanf(p_file, "%d\t%d\t%d\t%d\t", &p_arr_process[i].id,
+               &p_arr_process[i].arrivalTime, &p_arr_process[i].runTime,
+               &p_arr_process[i].priority) != 4) {
+      printf("Issue reading from file");
+    };
+  }
+
+  // Close File
+  fclose(p_file);
+
+  return p_arr_process;
+}
 
 ///==============================
-//don't mess with this variable//
-int * shmaddr;                 //
+// don't mess with this variable//
+int *shmaddr; //
 //===============================
 
-
-
-int getClk()
-{
-    return *shmaddr;
-}
-
+int getClk() { return *shmaddr; }
 
 /*
- * All process call this function at the beginning to establish communication between them and the clock module.
- * Again, remember that the clock is only emulation!
-*/
-void initClk()
-{
-    int shmid = shmget(SHKEY, 4, 0444);
-    while ((int)shmid == -1)
-    {
-        //Make sure that the clock exists
-        printf("Wait! The clock not initialized yet!\n");
-        sleep(1);
-        shmid = shmget(SHKEY, 4, 0444);
-    }
-    shmaddr = (int *) shmat(shmid, (void *)0, 0);
+ * All process call this function at the beginning to establish communication
+ * between them and the clock module. Again, remember that the clock is only
+ * emulation!
+ */
+void initClk() {
+  int shmid = shmget(SHKEY, 4, 0444);
+  while ((int)shmid == -1) {
+    // Make sure that the clock exists
+    printf("Wait! The clock not initialized yet!\n");
+    sleep(1);
+    shmid = shmget(SHKEY, 4, 0444);
+  }
+  shmaddr = (int *)shmat(shmid, (void *)0, 0);
 }
-
 
 /*
  * All process call this function at the end to release the communication
  * resources between them and the clock module.
  * Again, Remember that the clock is only emulation!
- * Input: terminateAll: a flag to indicate whether that this is the end of simulation.
- *                      It terminates the whole system and releases resources.
-*/
+ * Input: terminateAll: a flag to indicate whether that this is the end of
+ * simulation. It terminates the whole system and releases resources.
+ */
 
-void destroyClk(bool terminateAll)
-{
-    shmdt(shmaddr);
-    if (terminateAll)
-    {
-        killpg(getpgrp(), SIGINT);
-    }
+void destroyClk(bool terminateAll) {
+  shmdt(shmaddr);
+  if (terminateAll) {
+    killpg(getpgrp(), SIGINT);
+  }
 }
--- Project/process_generator.c
+++ Project/process_generator.c
@@ -2,111 +2,51 @@
 
 void clearResources(int);
 
-int main(int argc, char * argv[])
-{
-    signal(SIGINT, clearResources);
-    // TODO Initialization
-    // 1. Read the input files.
-    // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    #pragma region "User Input"
-        printf(
-        "1. SJF: Shortest Job First\n"
-        "2. PHPF: Preemptive Highest Priority First\n"
-        "3. RR: Round Robin\n\n"
-        "Choose the Scheduling Algorithm: ");
-        
-        enum schedulingAlgorithm schedulingAlg;
-        scanf("%u", &schedulingAlg);
-        
-        while (schedulingAlg!=SJF&&schedulingAlg!=PHPF&&schedulingAlg!=RR)
-        {
-        printf(
-        "Please select a valid option.\n"
-        "1. SJF: Shortest Job First\n"
-        "2. PHPF: Preemptive Highest Priority First\n"
-        "3. RR: Round Robin\n\n"
-        "Choose the Scheduling Algorithm: ");
-        scanf("%u", &schedulingAlg);
-        }
-        if(schedulingAlg==RR){
-            unsigned int quantum;
-            printf("Type the Quantum length: ");
-            scanf("%u", &quantum);
-        }
-    #pragma endregion 
-    
-    // 3. Initiate and create the scheduler and clock processes.
-    // 4. Use this function after creating the clock process to initialize clock
-    initClk();
-    // To get time use this
-    int x = getClk();
-    printf("current time is %d\n", x);
-    // TODO Generation Main Loop
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
-    destroyClk(true);
+int main(int argc, char *argv[]) {
+  signal(SIGINT, clearResources);
+// TODO Initialization
+// 1. Read the input files.
+// 2. Ask the user for the chosen scheduling algorithm and its parameters, if
+// there are any.
+#pragma region "User Input"
+  printf("1. SJF: Shortest Job First\n"
+         "2. PHPF: Preemptive Highest Priority First\n"
+         "3. RR: Round Robin\n\n"
+         "Choose the Scheduling Algorithm: ");
+
+  enum schedulingAlgorithm schedulingAlg;
+  scanf("%u", &schedulingAlg);
+
+  while (schedulingAlg != SJF && schedulingAlg != PHPF && schedulingAlg != RR) {
+    printf("Please select a valid option.\n"
+           "1. SJF: Shortest Job First\n"
+           "2. PHPF: Preemptive Highest Priority First\n"
+           "3. RR: Round Robin\n\n"
+           "Choose the Scheduling Algorithm: ");
+    scanf("%u", &schedulingAlg);
+  }
+
+  if (schedulingAlg == RR) {
+    unsigned int quantum;
+    printf("Type the Quantum length: ");
+    scanf("%u", &quantum);
+  }
+#pragma endregion
+
+  // 3. Initiate and create the scheduler and clock processes.
+  // 4. Use this function after creating the clock process to initialize clock
+  initClk();
+  // To get time use this
+  int x = getClk();
+  printf("current time is %d\n", x);
+  // TODO Generation Main Loop
+  // 5. Create a data structure for processes and provide it with its
+  // parameters.
+  // 6. Send the information to the scheduler at the appropriate time.
+  // 7. Clear clock resources
+  destroyClk(true);
 }
 
-void clearResources(int signum)
-{
-    //TODO Clears all resources in case of interruption
-}
-
-struct processData* load(char* inpFileName)
-{
-	int count_processes = 0;
-	struct processData* p_arr_process;
-	
-	//Opening File and checking if its been successfully opened 
-	FILE* p_file = fopen(inpFileName, "r");
-	if (p_file == NULL)
-	{
-		perror("ERROR HAS OCCURED IN INPUT FILE OPENINING");
-		fclose(p_file);
-		return NULL;
-	}
-
-
-	//Buffer needed to store content of line (This is needed even we dont need the content while counting the number of processes)
-	char buffer[256];
-	
-	//skipping past comments line
-	fgets(buffer, sizeof(buffer), p_file);
-
-	//itterating through the file to find number of processes
-	while (fgets(buffer, sizeof(buffer), p_file) != NULL) { count_processes += 1;  }
-
-	//once counting is complete we close the file to now read the file data 
-	fclose(p_file);
-
-	//Dyanmic allocation of array for processes
-	p_arr_process = (struct processData*) malloc(count_processes * sizeof(struct processData));
-
-
-	//Reopening input file
-	p_file = fopen(inpFileName, "r");
-	if (p_file == NULL)
-	{
-		perror("ERROR HAS OCCURED IN INPUT FILE OPENINING _2_");
-		fclose(p_file);
-		return NULL;
-	}
-
-	//skipping past comments line
-	fgets(buffer, sizeof(buffer), p_file);
-
-	//itterating through file to get data of each 
-	for (int i = 0; i < count_processes; i++)
-	{
-		if (fscanf(p_file, "%d\t%d\t%d\t%d\t", &p_arr_process[i].id, &p_arr_process[i].arrivalTime, &p_arr_process[i].runTime, &p_arr_process[i].priority) != 4) 
-		{
-			printf("Issue reading from file");
-		};
-	}
-		
-	//Close File
-	fclose(p_file);
-
-	return p_arr_process;
+void clearResources(int signum) {
+  // TODO Clears all resources in case of interruption
 }
