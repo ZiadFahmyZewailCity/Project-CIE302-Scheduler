a8a3aa0ad0369b5bea39d3df1d204df591edc3e2
Marwan <72122152+marwanbasemP3@users.noreply.github.com>
2024-11-22 17:39:04 +0200

    Close issue #23

--- Project/process.c
+++ Project/process.c
@@ -1,10 +1,7 @@
 #include "headers.h"
 #include <stdio.h>
 
-#define ID argv[1];
-#define PRIORITY argv[2];
-#define RUN_TIME argv[3];
-#define ARRIVAL argv[4];
+#define RUN_TIME argv[3]
 
 
 /* Modify this file as needed*/
@@ -13,17 +10,12 @@ int remainingtime;
 
 int main(int agrc, char * argv[])
 {
-    size_t pid = fork();
-
-    if(pid == 0)
-        execlp("./clk.out", "./clk.out");
-    
 
     initClk();
-    unsigned int start = getClk();
+    unsigned int startTime = getClk();
 
     int x = getClk();
-    remainingtime = atoi(argv[1]);
+    remainingtime = atoi(RUN_TIME);
     // it needs to get the remaining time from somewhere
     while (remainingtime > 0)
     {
