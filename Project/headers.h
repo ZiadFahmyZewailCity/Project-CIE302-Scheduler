1bfa503c76b6a6a55467aba2eb3151f01d994677
Ziad Fahmy <202201027@zewailcity.edu.eg>
2024-11-24 10:44:45 +0200

    fixes to ououtput function

--- Project/headers.h
+++ Project/headers.h
@@ -30,7 +30,7 @@ struct processData {
 
 enum schedulingAlgorithm { SJF = 1, PHPF, RR };
 
-enum state { running = 1, waiting = 2, started = 3, finished = 4 };
+enum state { running = 1, waiting = 2};
 
 struct PCB {
   enum state pstate;
@@ -48,6 +48,17 @@ struct processMsgBuff {
   long mtype;
   struct processData process;
 };
+
+struct processStateInfoMsgBuff {
+  unsigned int msgType;
+  unsigned int id;
+  unsigned int startTime;
+  unsigned int runTime;
+  unsigned int remainingTime;
+  unsigned int finishTime;
+  enum state pstate;
+};
+
 
 struct processFinalInfo {
   unsigned int msgType;
@@ -309,7 +320,7 @@ void destroyClk(bool terminateAll)
 
 FILE* p_out;
 
-void output(struct processData inpProcessData, int remainingTime,int currentTime)
+void output(struct processStateInfoMsgBuff inpProcessData ,int currentTime)
 {
     p_out = fopen("check.txt", "a");
     if (p_out == NULL)
@@ -323,31 +334,33 @@ void output(struct processData inpProcessData, int remainingTime,int currentTime
 
     //This checks if the update is that the process is terminated to print the extra parameters, 
     //number should be equal to the enum of terminated status
-    if(inpProcessData.pstate == finished)
+    if(inpProcessData.remainingTime == 0)
     {
         int turnAround = currentTime - inpProcessData.arrivalTime;
         int weightedTurnAround = (currentTime - inpProcessData.arrivalTime)/ inpProcessData.runTime;
 
         fprintf(p_out, "At \ttime %d \tprocess %d \tfinished\t arr %d \ttotal %d \tremain %d \twait %d\n \tTA %d \tWTA %d",
-        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, remainingTime, remainingTime, wait_time, turnAround, weightedTurnAround);
+        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, inpProcessData.runTime, inpProcessData.remainingTime, wait_time, turnAround, weightedTurnAround);
     }
     else
     {
         switch (inpProcessData.pstate)
-        {
-        case (started)
-            fprintf(p_out, "At \ttime %d \tprocess %d \tstarted\t arr %d \ttotal %d \tremain %d \twait %d\n" 
-        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, remainingTime, remainingTime, wait_time,);
-        break;
-        
+        {   
         case (running)
+          if (inpProcessData.runTime == inpProcessData.remainTime)
+          {
+            fprintf(p_out, "At \ttime %d \tprocess %d \tstarted\t arr %d \ttotal %d \tremain %d \twait %d\n" 
+            ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, inpProcessData.runTime, inpProcessData.remainingTime, wait_time,);
+          }
+          else
+          {
             fprintf(p_out, "At \ttime %d \tprocess %d \tresumed\t arr %d \ttotal %d \tremain %d \twait %d\n" 
-        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, remainingTime, remainingTime, wait_time,);
-        
+            ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, inpProcessData.runTime, inpProcessData.remainingTime, wait_time,);
+          }
         break;
         case (waiting)
             fprintf(p_out, "At \ttime %d \tprocess %d \tstopped arr %d \ttotal %d \tremain %d \twait %d\n" 
-        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, remainingTime, remainingTime, wait_time,);
+        ,currentTime, inpProcessData.id, inpProcessData.arrivalTime, inpProcessData.runTime, inpProcessData.remainingTime, wait_time,);
         break;
         
         default:
